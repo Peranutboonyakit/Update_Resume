137748d07003fc8d8033031d034f17a6f41fcb18
Peranut <peranut.pepo@hotmail.com>
2021-10-07 23:22:17 +0700

    o

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,75 +1,75 @@
-import React from 'react'
-import styled from 'styled-components'
-import ProjectBox from './ProjectBox'
-import exchange from '../img/Image/exchange.png'
-import resume1 from '../img/Image/resume1.jpg'
-import income from '../img/Image/income.png'
-import foodmenu from '../img/foodmenu.jpg'
-import movieapp from '../img/movieapp.jpg'
+import React from "react";
+import styled from "styled-components";
+import ProjectBox from "./ProjectBox";
+import exchange from "../img/Image/exchange.png";
+import resume1 from "../img/Image/resume1.jpg";
+import income from "../img/Image/income.png";
+import foodmenu from "../img/foodmenu.jpg";
+import movieapp from "../img/movieapp.jpg";
 
 const Projects = () => {
-    return (
-        <Container>
-            <Left>
-                <Text>
-                    <Title>Projects</Title>
-                </Text>
+  return (
+    <Container>
+      <Left>
+        <Text>
+          <Title>Projects</Title>
+        </Text>
 
-                <Item>
-                    <ProjectBox
-                        link='https://github.com/Peranutboonyakit/Food_Menu'
-                        img={foodmenu} />
-                    <ProjectBox
-                        link='https://github.com/Peranutboonyakit/Movie_axios'
-                        img={movieapp} />
-                    <ProjectBox
-                        link='https://peranutboonyakitt.vercel.app/'
-                        img={resume1} />
-                    <ProjectBox
-                        link='https://peranutboonyakit.github.io/ProjectJS/Exchange%20Money/index.html'
-                        img={exchange} />
-                    <ProjectBox
-                        link='https://peranutboonyakit.github.io/ProjectJS/Income%20and%20Expenses/index.html'
-                        img={income} />
-                </Item>
-            </Left>
-
-        </Container>
-    )
-}
+        <Item>
+          <ProjectBox
+            link="https://github.com/Peranutboonyakit/Food_Menu"
+            img={foodmenu}
+          />
+          <ProjectBox
+            link="https://github.com/Peranutboonyakit/Movie_axios"
+            img={movieapp}
+          />
+          <ProjectBox
+            link="https://peranutboonyakitt.vercel.app/"
+            img={resume1}
+          />
+          <ProjectBox
+            link="https://peranutboonyakit.github.io/ProjectJS/Exchange%20Money/index.html"
+            img={exchange}
+          />
+        </Item>
+      </Left>
+    </Container>
+  );
+};
 
 export default Projects;
 
 const Container = styled.div`
-    height: 100vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 const Left = styled.div`
-    width: 50%; 
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-`
+  width: 50%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
 const Text = styled.span`
-    margin-bottom: 50px;
-    @media only screen and (max-width: 680px){
-       margin-bottom: 0;
-    }
-`
+  margin-bottom: 50px;
+  @media only screen and (max-width: 680px) {
+    margin-bottom: 0;
+  }
+`;
 
 const Title = styled.span`
-    font-size: 70px;
-    font-weight: bold;
-    text-decoration: underline salmon;
-    font-style: italic;
-`
+  font-size: 70px;
+  font-weight: bold;
+  text-decoration: underline salmon;
+  font-style: italic;
+`;
 const Item = styled.span`
-    display: grid;
-    grid-template-columns: auto auto auto;
-    @media only screen and (max-width: 680px){
-        grid-template-columns: auto auto;
-    }
-`
+  display: grid;
+  grid-template-columns: auto auto auto;
+  @media only screen and (max-width: 680px) {
+    grid-template-columns: auto auto;
+  }
+`;
